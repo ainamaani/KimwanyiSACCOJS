2b841e6d17b55716697a56c4d72cce9307c7a546
Ainamaani Isaac <aina.isaac2002@gmail.com>
2023-10-17 08:07:49 +0300

    Added pagination

--- frontend/src/pages/MemberApplications.tsx
+++ frontend/src/pages/MemberApplications.tsx
@@ -1,4 +1,4 @@
-import { Button, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Tooltip, Typography } from "@mui/material";
+import { Button, IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, Tooltip, Typography } from "@mui/material";
 import { useEffect,useState } from "react";
 import axios from "axios";
 import useMemberApplicationContext from "../hooks/UseMemberApplicationContext";
@@ -29,6 +29,9 @@ const MemberApplications = ():JSX.Element => {
 
     const {applications, dispatch} = useMemberApplicationContext();
 
+    const [page,setPage] = useState<number>(0); //current page
+    const [rowsPerPage,setRowsPerPage] = useState<number>(5); //Rows per page
+
     useEffect(()=>{
         const fetchMemberApplications = async() =>{
             //try fetching member applications
@@ -51,6 +54,15 @@ const MemberApplications = ():JSX.Element => {
         console.log(applications);
     },[]);
 
+    const handleChangePage = (e: React.MouseEvent<HTMLButtonElement> | null, newPage:number) =>{
+        setPage(newPage);
+    }
+
+    const handleChangeRowsPage = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        setRowsPerPage(parseInt(e.target.value, 10));
+        setPage(0); //Reset to the first page when changing rows per page
+    }
+
     return ( 
         <div>
             { applications ? (
@@ -67,36 +79,48 @@ const MemberApplications = ():JSX.Element => {
                         </TableHead>
                         <TableBody>
                             {applications ? (
-                                applications.map((application : Application)=>(
-                                    <TableRow key={application._id}>
-                                        <TableCell>{application.firstName}</TableCell>
-                                        <TableCell>{application.lastName}</TableCell>
-                                        <TableCell>{application.gender}</TableCell>
-                                        <TableCell>{application.employmentStatus}</TableCell>
-                                        <TableCell>
-                                            <Tooltip title="View">
-                                                <IconButton color="primary" size="large" >
-                                                    <VisibilityRounded/>
-                                                </IconButton>
-                                            </Tooltip>
-                                            <Tooltip title="Approve">
-                                                <IconButton style={{color:'green'}} size="large" >
-                                                    <CheckCircleOutlineRounded />
-                                                </IconButton>
-                                            </Tooltip>
-                                            <Tooltip title="Reject">
-                                                <IconButton style={{color:'red'}} size="large">
-                                                    <CancelOutlined/>
-                                                </IconButton>
-                                            </Tooltip>
-                                        </TableCell>
-                                    </TableRow>
-                                ))
+                                applications
+                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    .map((application : Application)=>(
+                                        <TableRow key={application._id}>
+                                            <TableCell>{application.firstName}</TableCell>
+                                            <TableCell>{application.lastName}</TableCell>
+                                            <TableCell>{application.gender}</TableCell>
+                                            <TableCell>{application.employmentStatus}</TableCell>
+                                            <TableCell>
+                                                <Tooltip title="View">
+                                                    <IconButton color="primary" size="large" >
+                                                        <VisibilityRounded/>
+                                                    </IconButton>
+                                                </Tooltip>
+                                                <Tooltip title="Approve">
+                                                    <IconButton style={{color:'green'}} size="large" >
+                                                        <CheckCircleOutlineRounded />
+                                                    </IconButton>
+                                                </Tooltip>
+                                                <Tooltip title="Reject">
+                                                    <IconButton style={{color:'red'}} size="large">
+                                                        <CancelOutlined/>
+                                                    </IconButton>
+                                                </Tooltip>
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
                             ):(
                                 <Typography variant="h5">Loading data...</Typography>
                             )}
                         </TableBody>
                     </Table>
+                    <TablePagination 
+                        rowsPerPageOptions={[5,10,20]}
+                        component="div"
+                        count={applications.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPage}
+
+                    />
                 </TableContainer>
             ):(
                 <Typography variant="h5">
