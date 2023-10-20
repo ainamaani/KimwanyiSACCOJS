f8ebc092c9d18aca2a2982b9a27b8cdb3c5846bb
Ainamaani Isaac <aina.isaac2002@gmail.com>
2023-10-20 08:17:37 +0300

    Added pagination

--- frontend/src/pages/Members.tsx
+++ frontend/src/pages/Members.tsx
@@ -1,4 +1,5 @@
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
+import { Paper, Table, TableBody, TableCell, TableContainer, 
+    TableHead, TableRow, Typography, TablePagination } from "@mui/material";
 import React,{ useState,useEffect } from 'react';
 import axios from 'axios';
 
@@ -26,6 +27,8 @@ interface Member{
 
 const Members = ():JSX.Element => {
     const [members,setMembers] = useState<Member[] | null>(null);
+    const [page,setPage] = useState<number>(0); //current page
+    const [rowsPerPage,setRowsPerPage] = useState<number>(5); //Rows per page
 
     useEffect(()=>{
         const fetchApprovedMembers = async() =>{
@@ -42,6 +45,16 @@ const Members = ():JSX.Element => {
         }
         fetchApprovedMembers();
     },[members]);
+
+    const handleChangePage = (e: React.MouseEvent<HTMLButtonElement> | null, newPage:number) =>{
+        setPage(newPage);
+    }
+
+    const handleChangeRowsPage = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        setRowsPerPage(parseInt(e.target.value, 10));
+        setPage(0); //Reset to the first page when changing rows per page
+    }
+
     return (  
         <div>
             <Typography variant="h5">SACCO members</Typography>
@@ -57,7 +70,9 @@ const Members = ():JSX.Element => {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            { members.map((member : Member) =>(
+                            { members
+                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                .map((member : Member) =>(
                                 <TableRow key={member._id}>
                                     <TableCell>{member.firstName}</TableCell>
                                     <TableCell>{member.lastName}</TableCell>
@@ -67,6 +82,16 @@ const Members = ():JSX.Element => {
                             ))}
                         </TableBody>
                     </Table>
+                    <TablePagination 
+                        rowsPerPageOptions={[5,10,20]}
+                        component="div"
+                        count={members.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPage}
+
+                    />
                 </TableContainer>
             ):(
                 <Typography variant="h5" >Loading...</Typography>
