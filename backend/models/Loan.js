6a9a5379468fc7cc83d3195d6a7209e956c8b458
Ainamaani Isaac <aina.isaac2002@gmail.com>
2023-10-25 08:56:15 +0300

    Created model for loans

--- backend/models/Loan.js
+++ backend/models/Loan.js
@@ -7,6 +7,63 @@ const LoanSchema = new mongoose.Schema({
         ref: Member,
         required:true
     },
+    amountRequested:{
+        type:String,
+        required:[true, 'Amount requeted is required']
+    },
+    prefferedPaymentSchedule:{
+        type:String,
+        required:[true, 'Preffered payment schedule is required'],
+        enum:{
+            values: ['Monthly, Quarterly, Yearly'],
+            message: "The schedule must be one of 'monthly', 'quarterly' or 'yearly'"
+        } 
+    },
+    loanPurpose:{
+        type:String,
+        required:[true, 'Loan purpose is required'],
+    },
+    loanType:{
+        type:String,
+        required:[true, 'Loan type is required'],
+    },
+    monthlyIncome:{
+        type:String,
+        required:[true, 'Monthly income is required'],
+    },
+    otherSourcesOfIncome:{
+        type:String,
+        required:[true, 'Other sources of income required']
+    },
+    guarantorName:{
+        type:String,
+        required:[true, 'Guarantor Name is required']
+    },
+    guarantorEmail:{
+        type:String,
+        required:true,
+        validate: {
+            validator: async function (value){
+                // Check if the email format is valid
+                const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+                if (!emailRegex.test(value)) {
+                    throw new Error('Invalid email format. Please provide a valid email address.');
+                }
+            }
+        }
+    },
+    guarantorPhoneNumber:{
+        type:String,
+        required:true,
+        validate: {
+            validator: async function(value){
+                // Check if the phone number has at least 10 digits
+                if(value.length < 10){
+                    throw new Error('Phone number must be not less than 10 digits');
+                }
+            }
+        }
+    }
     
 
 },{timestamps:true});
